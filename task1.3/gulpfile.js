55614240a3a0f63f96ed72e986e87469b6b8f692
1ek <>
2021-03-11 14:22:06 +0400

    Working gulp, Media-mixins

--- task1.3/gulpfile.js
+++ task1.3/gulpfile.js
@@ -1,47 +1,53 @@
-let preprocessor = 'sass'
-
-const { src, dest, parallel, series, watch } = require('gulp')
-const browserSync = require('browser-sync').create()
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
-const cleancss = require('gulp-clean-css')
+const { src, dest, parallel, series, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleancss = require("gulp-clean-css");
+const del = require("del");
 
 function browsersync() {
-	browserSync.init({ // Инициализация Browsersync
-		server: { baseDir: 'app/' }, // Указываем папку сервера
-		notify: false, // Отключаем уведомления
-		online: true // Режим работы: true или false
-	})
+  browserSync.init({
+    // Инициализация Browsersync
+    server: { baseDir: "app/" }, // Указываем папку сервера
+    notify: false, // Отключаем уведомления
+    online: true, // Режим работы: true или false
+  });
 }
 
-function styles(){
-  return src('app/styles/scss' + preprocessor)
-  .pipe(autoprefixer({overrideBrowserslist:['last 10 version'],grid:true}))
-  .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } ))
-  .pipe(dest('app/styles/css/'))
-  .pipe(browserSync.stream())
+function styles() {
+  return src("app/styles/scss/style.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 10 version"], grid: true })
+    )
+    .pipe(
+      cleancss({
+        level: { 1: { specialComments: 0 } } /* , format: 'beautify' */,
+      })
+    )
+    .pipe(dest("app/styles/css/"))
+    .pipe(browserSync.stream());
 }
 
-function startwatch(){
-  watch('app/**/*' + preprocessor + '/**/*', styles)
-  watch('app/**/*').on ('change', browserSync.reload)
+function startwatch() {
+  watch("app/styles/scss/**/*.scss", styles);
+  watch("app/**/*.html").on("change", browserSync.reload);
 }
 
 function buildcopy() {
-	return src([
-		'app/styles/css/**/*.min.css',
-		'app/img/**/*',
-		'app/**/*.html',
-		], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
-	.pipe(dest('dist'))
+  return src(["app/styles/css/*.css", "app/img/**/*", "app/**/*.html"], {
+    base: "app",
+  }) // Параметр "base" сохраняет структуру проекта при копировании
+    .pipe(dest("dist"));
 }
 
-	function cleandist() {
-    return del('dist/**/*', { force: true })
+function cleandist(){
+	return del("dist/**/*", {force:true})
+
 }
 
 exports.browsersync = browsersync;
 exports.styles = styles;
 
+exports.dev = parallel(styles, browsersync, startwatch);
 exports.build = series(cleandist, styles, buildcopy);
-exports.default = parallel(styles, browsersync, startwatch);
