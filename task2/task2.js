75a9cfc954e9b4b017f4b60e0f1da74dbaa37624
1ek <>
2021-03-19 18:49:07 +0400

    arrayDiff and squareEveryDigits

--- task2/task2.js
+++ task2/task2.js
@@ -1,12 +1,28 @@
-/*
-arrayDiff = ([1, 2, 2, 3, 'abc', 'ab'], [ 2, 9, 99, 'abc']);
-function arrayDiff(a, b) {
-     return a.filter(e => !b.includes(e));
+function arrayDiff(array1, array2) {
+
+    return array1.filter(
+    function (calc){
+         return !array2.includes(calc)
+         //или через стрелочную функцию calc => !array2.includes(calc)
+    });
 }
 
-console.log ();
-*/
+function alphabetPosition (){
+
+}
+
+
+
+function squareEveryDigits(number){
+     return (('' + number).split('').map(val => Math.pow(val, 2)).join(''));
+}
 
 
 
 
+
+
+
+
+console.log (arrayDiff ([1, 2, 2, 3, 'abc', 'ab'], [ 2, 9, 99, 'abc']));
+console.log (squareEveryDigits (9119));
