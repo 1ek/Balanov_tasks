1546e7a89e6df0ba230a4acb0357e73e009aec2f
1ek <>
2021-03-20 16:17:01 +0400

    Ver.1.0 (everything works)

--- task2/task2.js
+++ task2/task2.js
@@ -1,27 +1,15 @@
 function arrayDiff(array1, array2) {
-
-    return array1.filter(calc => !array2.includes(calc)
-/*  function (calc){
-         return !array2.includes(calc)
-     }
-*/
-    );
+  return array1.filter((val) => !array2.includes(val));
 }
 
-function alphabetPosition (){
-
+function alphabetPosition(text) {
+  return text.toLowerCase().replace(/[^a-z]/gi, "").replace(/[a-z]/gi, val => val.charCodeAt() - 96 + " ").trim();
 }
 
-
-
-function squareEveryDigits(number){
-     return (('' + number).split('').map(val => Math.pow(val, 2)).join(''));
+function squareEveryDigits(number) {
+  return ("" + number).split("").map((val) => Math.pow(val, 2)).join("");
 }
 
-
-
-
-
-
-console.log (arrayDiff ([1, 2, 2, 3, 'abc', 'ab'], [ 2, 9, 99, 'abc']));
-console.log (squareEveryDigits (9119));
+console.log(arrayDiff([1, 2, 2, 3, "abc", "ab"], [2, 9, 99, "abc"]));
+console.log(alphabetPosition("Aa Bb The sunset sets at twelve o' clock."));
+console.log(squareEveryDigits(9119));
